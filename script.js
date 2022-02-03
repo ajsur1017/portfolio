94feb68880113a54be92f7ff8b559a5febdd232b
Anthony <ajsur1017@gmail.com>
2022-02-03 15:19:44 -0500

    portfolio

--- script.js
+++ script.js
@@ -6,32 +6,46 @@ const sucess = document.querySelector("#sucess");
 const errorNodes = document.querySelectorAll(".error");
 
 // Validate data
-function validateForm(){
+function validateForm() {
 
     clearMessages();
     let errorFlag = false
 
-    if (nameInput.value.length < 1){
+    if (nameInput.value.length < 1) {
         errorNodes[0].innerText = "Name cannot be blank";
         nameInput.classList.add("error-border")
         errorFlag = true;
     }
-    if (!emailIsValid(email.value)){
+    if (!emailIsValid(email.value)) {
         errorNodes[1].innerText = "Invalid Email adress";
         email.classList.add("error-border");
         errorFlag = true;
     }
-    if (message.value.length < 1){
+    if (message.value.length < 1) {
         errorNodes[2].innerText = "Please enter a message";
         message.classList.add("error-border");
         errorFlag = true;
     }
-    if (!errorFlag){
+    if (!errorFlag) {
         sucess.innerText = "Success!"
     }
 
+    // // https://api.jquery.com/jQuery.ajax
+    // $.ajax({
+    //     method: 'POST',
+    //     url: 'https://formsubmit.co/ajax/ajsur1017@gmail.com',
+    //     dataType: 'json',
+    //     accepts: 'application/json',
+    //     data: {
+    //         name: "FormSubmit",
+    //         message: "FormSubmit"
+    //     },
+    //     success: (data) => console.log(data),
+    //     error: (err) => console.log(err)
+    // });
+
 }
-function clearMessages(){
+function clearMessages() {
     for (let i = 0; i < errorNodes.length; i++) {
         errorNodes[i].innerText = "";
     }
@@ -40,7 +54,7 @@ function clearMessages(){
     email.classList.remove("error-border");
     message.classList.remove("error-border");
 }
-function emailIsValid(email){
+function emailIsValid(email) {
     let pattern = /\S+@\S+\.\S+/;
     return pattern.test(email);
 }
@@ -48,11 +62,12 @@ function emailIsValid(email){
 // Show and Hide contact form.
 var a;
 function show_hide_overlay() {
-    if(a==0) {
-        document.getElementById("overlay").style.display="none";
-        return a=1
+    if (a == 0) {
+        document.getElementById("overlay").style.display = "none";
+        return a = 1
     } else {
-        document.getElementById("overlay").style.display="inline";
-        return a=0
+        document.getElementById("overlay").style.display = "inline";
+        return a = 0
     }
 }
+
